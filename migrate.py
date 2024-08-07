a2723ed55d174d88cc3f496a0f24da6739766ce0
rue1-suzuki <suzuki.ryuichi@formzu.co.jp>
2024-08-07 17:58:21 +0900

    マイグレーション時のloggingの文言を修正

--- migrate.py
+++ migrate.py
@@ -1,11 +1,14 @@
+import logging
+
 from database import engine
 from models.item_model import Base as ItemBase
 
 
 def migrate():
-    print("Creating item_tables")
+    logging.warning("item_model migration")
     ItemBase.metadata.create_all(bind=engine)
-    print("item_tables created")
+
+    logging.warning("migration complete")
 
 
 if __name__ == "__main__":
