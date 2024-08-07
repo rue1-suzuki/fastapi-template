3bc3628f7666cf7818bae56f61ddcda89a351e77
rue1-suzuki <suzuki.ryuichi@formzu.co.jp>
2024-08-07 15:08:32 +0900

    uvicorn起動コマンドをmain.pyに内包

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
+import uvicorn
 from fastapi import FastAPI
 
 from routers.item_router import item_router
 
 app = FastAPI()
 app.include_router(router=item_router)
+
+
+# uvicorn起動コマンド 本番用
+if __name__ == "__main__":
+    uvicorn.run(
+        app=app,
+        host="0.0.0.0",
+        port=8000,
+        reload=False,
+    )
